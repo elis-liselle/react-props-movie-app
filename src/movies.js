608b29d5d31f1dab0fbd03da677784d2e90079b7
Elis-Liselle Soosalu <elis.liselle@gmail.com>
2022-03-28 23:08:23 +0300

    DRY-removed code that repeated

---
+++ src/movies.js
@@ -0,0 +1,28 @@
+let movies = [
+  {
+    id: "1",
+    movieName: "Inside Out",
+    image:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lRHE0vzf3oYJrhbsHXjIkF4Tl5A.jpg",
+  },
+  {
+    id: "2",
+    movieName: "Sing",
+    image:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zZTlF2eVVUkbdmccd3bNUU9T9sD.jpg",
+  },
+  {
+    id: "3",
+    movieName: "Zootopia",
+    image:
+      "https://www.themoviedb.org/t/p/w1280/hlK0e0wAQ3VLuJcsfIYPvb4JVud.jpg",
+  },
+  {
+    id: "4",
+    movieName: "Tom and Jerry: Cowboy Up!",
+    image:
+      "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/muIaHotSaSUQr0KZCIJOYQEe7y2.jpg",
+  },
+];
+
+export default movies;
